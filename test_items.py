0020191d612f8cf50131aa8cfc497e0605a529a7
ahranat <granatov58@gmail.com>
2023-09-18 16:14:34 +0200

    added to cart button is displayed

--- test_items.py
+++ test_items.py
@@ -6,7 +6,8 @@ link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
 
 def test_add_to_cart_button(browser):
     browser.get(link)
-    assert browser.find_element(By.CLASS_NAME, 'btn-add-to-basket'), 'There is no add to cart button'
-
+    add_to_cart_button = browser.find_element(By.CLASS_NAME, 'btn-add-to-basket')
+    assert add_to_cart_button.is_displayed(), 'There is no add to cart button'
+    
 if __name__ == "__main__":
     pytest.main()
